6e41b943c41ce738fdc322e1bb14db8b7dc3ae0e
Arjun Kishore <arjunkishore005@gmail.com>
2026-09-26 12:32:31 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i not in nums:
+                return i
+            
+        return len(nums)
